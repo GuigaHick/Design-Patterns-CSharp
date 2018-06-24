f148cf42c7f7471bbbd5cdd5966dd21ccf592be0
guigahick <guilherme.eremcdtpa@gmail.com>
2018-06-24 18:43:05 -0300

    initial commit

---
+++ SingleResponsability/SingleResponsability/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsability
+{
+    public class Journal
+    {
+
+        private readonly List<string>
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
