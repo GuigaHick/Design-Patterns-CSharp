d6fbbf48f2e6fe9536979dcd564095a0249b33ad
guigahick <guilherme.eremcdtpa@gmail.com>
2018-08-11 07:45:23 -0300

    Add SOLID principles for design pattern

---
+++ SingleResponsability/InterfaceSegregation/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregation
+{
+    class Program
+    {
+
+        public class Document
+        {
+
+        }
+        //BAD CHOICE 
+
+        public interface IMachine
+        {
+            void Print(Document D);
+            void Scan(Document D);
+            void Fax(Document D);
+        }
+
+        public class MultiFunctionPrinter : IMachine
+        {
+            public void Fax(Document D)
+            {
+                //
+            }
+
+            public void Print(Document D)
+            {
+                //
+            }
+
+            public void Scan(Document D)
+            {
+                //
+            }
+        }
+        //but for a simple printer we would have methods "remaining"..
+        public class OldFashionedPrinter : IMachine
+        {
+            public void Print(Document d)
+            {
+                // yep
+            }
+
+            public void Fax(Document d)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public void Scan(Document d)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+        //GOOD CHOICE
+        public interface IPrinter
+        {
+            void Print(Document D);
+        }
+
+        public interface IScanner
+        {
+            void Scanner(Document D);
+        }
+
+        public interface IMultiFunctionDevice : IPrinter, IScanner
+        {
+
+        }
+
+        public class MultiFunctionMachine : IMultiFunctionDevice
+        {
+            private IPrinter printer;
+            private IScanner scanner;
+
+            public MultiFunctionMachine( IPrinter printer, IScanner scanner)
+            {
+                if(printer == null)
+                {
+                    throw new ArgumentNullException();
+                }
+                if(scanner == null)
+                {
+                    throw new ArgumentNullException();
+                }
+                this.printer = printer;
+                this.scanner = scanner;
+            }
+
+
+            public void Print(Document D)
+            {
+                printer.Print(D);
+            }
+
+            public void Scanner(Document D)
+            {
+                scanner.Scanner(D);
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ SingleResponsability/LiskovSubstitution/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitution
+{
+    class Program
+    {
+        public class Rectangle
+        {
+            public virtual int Width { get; set; }
+            public virtual int Height { get; set; }
+
+            public Rectangle()
+            {
+
+            }
+
+            public Rectangle(int width, int height)
+            {
+                this.Width = width;
+                this.Height = height;
+            }
+
+            public override string ToString()
+            {
+                return $"{nameof(Width) }:{Width},{nameof(Height)} : {Height}";
+            }
+        }
+
+        public class Square : Rectangle
+        {
+           public override int Width
+            {
+                set { base.Height = base.Width = value; }
+            }
+
+            public override int Height
+            {
+                set { base.Height = base.Width = value; }
+            }
+        }
+
+        static int Area(Rectangle r) => r.Width * r.Height;
+
+        static void Main(string[] args)
+        {
+            Rectangle rc = new Rectangle(2, 3);
+            Console.WriteLine($"{rc} has Area {Area(rc)}");
+
+            Rectangle sq = new Square();
+            sq.Width = 4;
+            Console.WriteLine($"{sq} has Area {Area(sq)}");
+
+            Console.ReadKey();
+        }
+    }
+}
