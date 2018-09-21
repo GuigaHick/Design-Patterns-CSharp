2fd97f5332d89bd9c03d71a1a8349c6a07d736c5
= <=>
2018-09-21 07:29:35 -0300

    Add Demos using Builder and Factory

---
+++ SingleResponsability/DemoBuilder/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoBuilder
+{
+    public class HtmlElement
+    {
+        public string Name, Text;
+        public List<HtmlElement> Elements = new List<HtmlElement>();
+        private const int indentSize = 2;
+
+        public HtmlElement()
+        {
+
+        }
+
+        public HtmlElement(string Name, string Text)
+        {
+            this.Name = Name ?? throw new ArgumentNullException(paramName:nameof(Name));
+            this.Text = Text ?? throw new ArgumentNullException(paramName:nameof(Text));
+        }
+
+        private string ToStringImple(int indent)
+        {
+            var sb = new StringBuilder();
+            var i = new string(' ', indentSize * indent);
+
+            sb.AppendLine($"{i} <{Name}>");
+
+            if(!string.IsNullOrWhiteSpace(Text))
+            {
+                sb.Append(new string(' ', indentSize * (indent + 1)));
+                sb.AppendLine(Text);
+                sb.Append("\n");
+            }
+
+            foreach(var e in Elements)
+            {
+                sb.Append(e.ToStringImple(indent + 1));
+            }
+
+            sb.AppendLine($"{i}</{Name}>");
+
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return ToStringImple(0);
+        }
+    }
+
+    public class HtmlBuilder
+    {
+        HtmlElement root = new HtmlElement();
+        private string rootName;
+
+
+        public HtmlBuilder(string rootName)
+        {
+            this.rootName = rootName;
+            root.Name = rootName;
+        }
+        //Not Fluent
+        public void AddChild(string childName, string childText)
+        {
+            var e = new HtmlElement(childName, childText);
+            root.Elements.Add(e);
+        }
+
+        //Fluent
+        public HtmlBuilder AddChildFluent(string childName, string childText)
+        {
+            var e = new HtmlElement(childName, childText);
+            root.Elements.Add(e);
+
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return root.ToString();
+        }
+
+        public void Clear()
+        {
+            root = new HtmlElement { Name = rootName };
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Poor Approach
+            // if you want to build a simple HTML paragraph using StringBuilder
+            var hello = "hello";
+            var sb = new StringBuilder();
+            sb.Append("<p>");
+            sb.Append(hello);
+            sb.Append("</p>");
+            Console.WriteLine(sb);
+
+            // now I want an HTML list with 2 words in it
+            var words = new[] { "hello", "world" };
+            sb.Clear();
+            sb.Append("<ul>");
+            foreach (var word in words)
+            {
+                sb.AppendFormat("<li>{0}</li>", word);
+            }
+            sb.Append("</ul>");
+            Console.WriteLine(sb);
+
+
+
+            //Using Builder
+            var builder = new HtmlBuilder("ul");
+            builder.AddChild("li", "Hello");
+            builder.AddChild("li", "World");
+
+            Console.WriteLine(builder.ToString());
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ SingleResponsability/DemoFactory/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoFactory
+{
+    public class Point
+    {
+        private double x;
+        private double y;
+
+        //Factory Method
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+            
+        //Factory Method
+        public static Point NewCartesianPoint(int x, int y)
+        {
+            return new Point(x, y);
+        }
+
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return $" {nameof(x)}: {x},{nameof(y)} : {y}";
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var polarPoint = Point.NewPolarPoint(1, Math.PI / 2);
+            Console.WriteLine(polarPoint.ToString());
+
+            var cartesianPoint = Point.NewCartesianPoint(1,2);
+            Console.WriteLine(cartesianPoint.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
