b8594b84280a8dd8718985f22caa8a693f7466a9
= <=>
2018-10-06 17:24:03 -0300

    Add Demos using composite and Observer using event keyword

---
+++ SingleResponsability/DemoObserver/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoObserver
+{
+    public class FalssIllEventArgs 
+    {
+        public string Address;
+    }
+
+
+    public class Person
+    {
+        public void CatchACold()
+        {
+            FalssIll?.Invoke(this,  new FalssIllEventArgs { Address = "123 London Road" });
+        }
+        public event EventHandler<FalssIllEventArgs> FalssIll;
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.FalssIll += CallADoctor;
+
+            Console.WriteLine("Person's health is ok.If you press a key a person gonna catch a cold");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("Person caught a cold");
+            p.CatchACold();
+            Console.ReadKey();
+        }
+
+        private static void CallADoctor(object sender, FalssIllEventArgs e)
+        {
+            Console.WriteLine($"A doctor has been called to { e.Address}");
+        }
+    }
+}
